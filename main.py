9e5cb99118f2508718cd6345e7d1ad0653f89ffe
Abhishek <122111262+danialjohan@users.noreply.github.com>
2023-03-17 06:39:26 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#Made by Abhishek
+print("FIND AND REPLACE")
+n = input("Enter your Paragraph")
+while n:
+  f = input("Find What: ")
+  r = input("Replace with: ")
+  new = n.replace(f,r)
+  if new == n;
+     print(f"{f} not found\n")
+  else:
+     print("Replaced Successfully.\n")
+     print(new)
+  n = new
